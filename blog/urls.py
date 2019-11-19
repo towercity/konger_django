9717aff58eca45d8d23b3b87772028a429c3b291
Matt Nerger <matthewjnerger3@gmail.com>
2019-11-19 16:40:51 -0500

    repositoina gain

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail')
+    path('blog/', views.PostList.as_view(), name='post_list'),
+    path('blog/<slug:slug>/', views.PostDetail.as_view(), name='post_detail')
 ]
