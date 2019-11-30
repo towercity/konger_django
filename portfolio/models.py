dc699e782f969b83c58ce40d7c1b41eb1369a903
Matt Nerger <matthewjnerger3@gmail.com>
2019-11-30 12:52:17 -0500

    create project and languge models

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Language(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self): 
+        return self.name
+
+
+class Project(models.Model):
+    DISPLAY = (
+        (0, "Hide"),
+        (1, "Show")
+    )
+
+    # Basic info
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    short_description = models.TextField()
+    logo = models.ImageField(upload_to='project_images')
+
+    # Optionals
+    link = models.URLField(max_length=200, blank=True)
+    screenshot = models.ImageField(upload_to='project_images', blank=True)
+    full_description = models.TextField(blank=True)
+
+    # Meta stuff
+    languages = models.ManyToManyField(Language)
+    display = models.IntegerField(choices=DISPLAY)
+    built_date = models.DateTimeField()
+
+    def __str__(self): 
+        return self.title
+
+    class Meta:
+        ordering = ['-built_date']
