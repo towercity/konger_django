a8c111844bebba409410a50f5f4a1eaeec55164b
Matt Nerger <matthewjnerger3@gmail.com>
2019-11-29 20:02:37 -0500

    Add site id

--- nerger_portfolio/settings.py
+++ nerger_portfolio/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+SITE_ID = 1 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -39,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
     'blog',
 ]
 
