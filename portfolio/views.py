c6324d93bf06d2597df234f862e09adfbe22f63f
Matt Nerger <matthewjnerger3@gmail.com>
2019-12-01 15:36:45 -0500

    Allow both projects and language list

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Language
+from .models import Language, Project
+
 
 # Create your views here.
 class Index(generic.ListView):
-    queryset = Language.objects.filter(show_in_bar=1)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['language_list'] = Language.objects.filter(show_in_bar=1)
+        return context
+
+    queryset = Project.objects.filter(display=1)
+    context_object_name = 'project_list'
     template_name = 'home.html'
