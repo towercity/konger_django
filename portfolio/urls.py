2d47ffcb8a1c7381ad0eaec9f48909b6b0c94bc9
Matt Nerger <matthewjnerger3@gmail.com>
2019-12-01 15:38:18 -0500

    Add home view

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,4 +5,5 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', views.Index.as_view(), name='lang_list'),
+    path('home', views.Index.as_view(), name='lang_list'),
 ]
