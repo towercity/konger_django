724364899e0e0b7671ed33b9678b3ec349291a4e
Matt Nerger <matthewjnerger3@gmail.com>
2019-12-19 17:58:46 -0500

    Add pagination

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from .models import Post, Page, Publication
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog/post_list.html'
+    paginate_by = 3
 
 class PostDetail(generic.DetailView):
     model = Post
